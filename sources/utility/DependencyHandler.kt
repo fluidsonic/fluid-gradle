3acd5dd8298d14b2e4413ed2ea034c93a89504d1
Marc Knaup <marc@knaup.io>
2019-05-09 12:12:02 +0100

    added more dependency configurations

--- sources/utility/DependencyHandler.kt
+++ sources/utility/DependencyHandler.kt
@@ -7,11 +7,31 @@ fun DependencyHandler.api(dependencyNotation: Any) =
 	add("api", dependencyNotation)
 
 
+fun DependencyHandler.testApi(dependencyNotation: Any) =
+	add("testApi", dependencyNotation)
+
+
 @Suppress("unused")
 fun DependencyHandler.fluid(name: String, version: String) =
 	"com.github.fluidsonic:fluid-$name:$version"
 
 
+fun DependencyHandler.implementation(dependencyNotation: Any) =
+	add("implementation", dependencyNotation)
+
+
 @Suppress("unused")
 fun DependencyHandler.kotlinx(name: String, version: String) =
 	"org.jetbrains.kotlinx:kotlinx-$name:$version"
+
+
+fun DependencyHandler.runtimeOnly(dependencyNotation: Any) =
+	add("runtimeOnly", dependencyNotation)
+
+
+fun DependencyHandler.testImplementation(dependencyNotation: Any) =
+	add("testImplementation", dependencyNotation)
+
+
+fun DependencyHandler.testRuntimeOnly(dependencyNotation: Any) =
+	add("testRuntimeOnly", dependencyNotation)
