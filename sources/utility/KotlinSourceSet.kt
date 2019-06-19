c5a20fd5db7bb4121ec70c6f36b495d9a6acddaf
Marc Knaup <marc@knaup.io>
2019-06-19 20:35:35 +0100

    added more extensions to NamedDomainObjectContainer<KotlinSourceSet>

--- sources/utility/KotlinSourceSet.kt
+++ sources/utility/KotlinSourceSet.kt
@@ -13,12 +13,36 @@ val NamedDomainObjectContainer<KotlinSourceSet>.commonTest
 	get() = named<KotlinSourceSet>("commonTest")
 
 
+val NamedDomainObjectContainer<KotlinSourceSet>.iosArm64Main
+	get() = named<KotlinSourceSet>("iosArm64Main")
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.iosArm64Test
+	get() = named<KotlinSourceSet>("iosArm64Test")
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.iosX64Main
+	get() = named<KotlinSourceSet>("iosX64Main")
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.iosX64Test
+	get() = named<KotlinSourceSet>("iosX64Test")
+
+
 val NamedDomainObjectContainer<KotlinSourceSet>.jvmMain
 	get() = named<KotlinSourceSet>("jvmMain")
 
 
 val NamedDomainObjectContainer<KotlinSourceSet>.jvmTest
 	get() = named<KotlinSourceSet>("jvmTest")
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.macosX64Main
+	get() = named<KotlinSourceSet>("macosX64Main")
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.macosX64Test
+	get() = named<KotlinSourceSet>("macosX64Test")
 
 
 val NamedDomainObjectContainer<KotlinSourceSet>.objcMain
