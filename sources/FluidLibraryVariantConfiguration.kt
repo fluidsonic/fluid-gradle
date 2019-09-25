20620e31b3c25f4a20a7039e32aa17e6d1d8f6c8
Marc Knaup <marc@knaup.io>
2019-09-25 18:55:05 +0800

    Updated more dependencies

--- sources/FluidLibraryVariantConfiguration.kt
+++ sources/FluidLibraryVariantConfiguration.kt
@@ -192,10 +192,10 @@ class FluidLibraryVariantConfiguration private constructor(
 
 						dependencies {
 							implementation(kotlin("test-junit5"))
-							implementation("org.junit.jupiter:junit-jupiter-api:5.5.0")
+							implementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
 
-							runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.0")
-							runtimeOnly("org.junit.platform:junit-platform-runner:1.5.0")
+							runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+							runtimeOnly("org.junit.platform:junit-platform-runner:1.5.2")
 						}
 					}
 
