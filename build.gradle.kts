65683b6c53f9115d27c718b31f296f20ce27b598
Marc Knaup <marc@knaup.io>
2019-10-18 17:27:38 +0800

    🎉 0.9.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.plugin.*
 
 description = "Gradle plugin used to simplify configuration of all com.github.fluidsonic.* Kotlin libraries"
 group = "com.github.fluidsonic.fluid-library"
-version = "0.9.29"
+version = "0.9.30"
 
 plugins {
 	`java-gradle-plugin`
