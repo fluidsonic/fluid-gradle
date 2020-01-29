4b369f0a74438e442f567bbae8e7820153ed9987
Marc Knaup <marc@knaup.io>
2020-01-29 16:32:08 +0700

    Gradle 6.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ tasks {
 
 	withType<Wrapper> {
 		distributionType = Wrapper.DistributionType.ALL
-		gradleVersion = "6.1"
+		gradleVersion = "6.1.1"
 	}
 }
 
--- sources/Versions.kt
+++ sources/Versions.kt
@@ -5,5 +5,5 @@ internal object Versions {
 
 	const val junitPlatform = "1.6.0"
 	const val junitJupiter = "5.6.0"
-	const val gradle = "6.1"
+	const val gradle = "6.1.1"
 }
