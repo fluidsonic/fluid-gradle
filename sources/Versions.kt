ff5d73179c439acb287c28f55a2f4835c916674f
Marc Knaup <marc@knaup.io>
2021-02-08 02:27:33 +0100

    JUnit 5.7.1

--- sources/Versions.kt
+++ sources/Versions.kt
@@ -3,7 +3,7 @@ package io.fluidsonic.gradle
 
 internal object Versions {
 
-	const val junitPlatform = "1.7.0"
-	const val junitJupiter = "5.7.0"
+	const val junitPlatform = "1.7.1"
+	const val junitJupiter = "5.7.1"
 	const val gradle = "6.8.2"
 }
