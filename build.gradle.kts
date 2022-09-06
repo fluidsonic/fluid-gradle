75b62bfded1e50b37e71a5f243f62c04ff7af2a3
Marc Knaup <marc@knaup.io>
2022-09-06 03:12:55 +0200

    🎉 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.*
 
 description = "Gradle plugin for simplifying the configuration of io.fluidsonic.* Kotlin libraries"
 group = "io.fluidsonic.gradle"
-version = "1.2.0"
+version = "1.2.1"
 
 plugins {
 	`java-gradle-plugin`
